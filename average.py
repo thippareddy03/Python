ffa0e7193887c282c178df70486f7886f1d971f4
VTR <thippareddy1997@gmail.com>
2025-05-12 16:25:42 -0600

    Adding average of marks code

---
+++ average.py
@@ -0,0 +1,18 @@
+maths = -1
+physics = -2
+chemistry = -3
+average = (maths + physics + chemistry) / 3
+#print(average) and need to print in which grade he was assigned to.
+
+if average <= 100 and average >= 90:
+    print("Grade A")
+elif average < 90 and average >= 80:
+    print("Grade B")
+elif average < 80 and average >= 70:
+    print("Grade C")
+elif average < 70 and average >= 60:
+    print("Grade D")
+elif average < 60 and average >= 0:
+    print("Grade E")
+else:
+    print ("Marks are uncontrollable")
