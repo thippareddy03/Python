ac2e212ade4670d406804d687d73e7d22d81aade
VTR <thippareddy1997@gmail.com>
2025-05-12 23:54:06 -0600

    Added factorial and reverse

--- printnumbers.py
+++ printnumbers.py
@@ -1,9 +1,15 @@
-#Sum of numbers from 1 to 10
+#Print numbers from 1 to 10
 
+Number = 1
+while Number <= 10:
+    print (Number)
+    Number += 1
+
+#Sum of numbers from 1 to 10
 number = 1
 sum = 0
 while number <= 10:
-    sum =+ number
+    sum += number
     number += 1
 print(sum)
 
---
+++ reversenumber.py
@@ -0,0 +1,9 @@
+#Factorial of a number
+Number = 10
+Factorial = 1
+while Number > 0:
+    Factorial *= Number
+    Number -= 1
+print(Factorial)
+
+    
