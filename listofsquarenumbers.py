b5ac8fe19867803dc3cf4748567fccfef2bafe98
VTR <thippareddy1997@gmail.com>
2025-05-21 21:37:00 -0600

    Added vowels

---
+++ listofsquarenumbers.py
@@ -0,0 +1,22 @@
+"""
+Create a list of squares of numbers from 1 to 15.
+"""
+
+for number in range (1, 16):
+    result = number * number
+    print(result)
+
+"""
+Count the number of vowels in a given sentence.
+"""
+
+Sentence = "Count the number of vowels in a given sentence"
+
+vowels = "aeiouAEIOU"
+count = 0
+
+for char in Sentence:
+    if char in vowels:
+        print(char)
+        count += 1
+print("Number of vowels in the sentence:", count)
